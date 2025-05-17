10949a7703f191bbba6c68572545389fb20f520c
imane <rhesri.1337@gmail.com>
2025-05-17 15:16:23 +0200

    utils

---
+++ internal/controller/utils/utils.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	kaasv1 "github.com/faithByte/kaas/api/v1"
+	corev1 "k8s.io/api/core/v1"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const MY_NAMESPACE = "default"
+const APIVersion = "faithbyte.kaas/v1"
+const KIND = "JobSteps"
+
+var Log = log.Log
+
+type Status int8
+
+const (
+	NotStarted       Status = 0
+	ComputesCreating Status = 1
+	ComputesCreated  Status = 2
+	Launched         Status = 3
+	Completed        Status = 4
+	Error            Status = 5
+)
+
+type ReconcilerData struct {
+	Job     kaasv1.JobSteps
+	Client  client.Client
+	Context context.Context
+	Scheme  *runtime.Scheme
+}
+
+// func isOwnedByMe(obj *metav1.Object, uid string) bool {
+// 	controller := metav1.GetControllerOf(*obj)
+
+// 	if string(controller.UID) == uid {
+// 		return true
+// 	}
+// 	return false
+// }
+
+func GetPodsOwnerUID(pod *corev1.Pod) string {
+	ref := pod.OwnerReferences[0]
+
+	if ref.APIVersion == APIVersion && ref.Kind == KIND {
+		return string(ref.UID)
+	}
+	return ""
+}
